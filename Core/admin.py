34ab1add9d8a13a44b3faf215b319b32a7b445cc
MultyPrism <76118715+darkdragon228@users.noreply.github.com>
2024-03-14 07:39:35 +0200

    1.1 update

--- Core/admin.py
+++ Core/admin.py
@@ -3,22 +3,38 @@ import Core.SqlInterface
 
 def admin():
 
-    if request.remote_addr in session and session[request.remote_addr]["type"] == "admin":
-        return render_template("admin.html")
+    return render_template("admin.html")
 
-    return "", 404
 
 def setConf():    
-    if request.remote_addr in session and session[request.remote_addr]["type"] == "admin":
+    if request.remote_addr in session:
         data = request.get_json()
 
-        response = Core.SqlInterface.SetQuezConf(data)
+        response = Core.SqlInterface.SetQuezConf(data, session[request.remote_addr]["name"])
 
         if response:
             return ""
 
     return "", 404
 
+def AdminQuiz():
+
+    if request.remote_addr in session:
+    
+        return {"response":Core.SqlInterface.GetUserQuiz(session[request.remote_addr]["name"])}
+    
+    return "", 401
+
+def GetCode(quiz):
+
+    if request.remote_addr in session:
+    
+        return {"response":Core.SqlInterface.GetCode(session[request.remote_addr]["name"], quiz)}
+    
+    return "", 401
+
 def activate(app):
-    app.add_url_rule("/admin", "admin", admin)
+    app.add_url_rule("/admin.html", "admin", admin)
     app.add_url_rule("/admin/setconf", "adminconf", setConf, methods=["POST"])
+    app.add_url_rule("/admin/quizlist", "getAdminQuiz", AdminQuiz)
+    app.add_url_rule("/admin/quizcode/<quiz>", "getcode", GetCode)
--- Core/quizService.py
+++ Core/quizService.py
@@ -17,7 +17,15 @@ def Question(quiz):
     else:
         return "", 404
 
+def CodeQuestion(quiz):
+    response = Core.SqlInterface.GetQuestion(quiz)
+
+    if response:    
+        return {"response": response}
+    else:
+        return "", 404
+
 def activate(app):
-    app.add_url_rule("/quiz", "quizhtml", quiz)
-    app.add_url_rule("/get/quizlist", "getQuiz", Quiz)
+    app.add_url_rule("/quiz.html", "quizhtml", quiz)
+    #app.add_url_rule("/get/quizlist", "getQuiz", Quiz)
     app.add_url_rule("/get/question/<quiz>", "getQuestion", Question)
